6e79fb5906088a92a3e8adab616e4f5e2473b111
Rajput Nishtha <rajputnishtha029@gmail.com>
2026-09-24 19:43:16 +0530

    Fix express route path

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.post('/api/payment/verify', (req, res) => {
 const path = require('path');
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
